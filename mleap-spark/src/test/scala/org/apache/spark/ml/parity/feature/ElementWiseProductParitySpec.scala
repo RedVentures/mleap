7937d7aa538cc3ef5f8eac9f6196daf13f29870d
asarb <asarb@expedia.com>
2019-04-29 12:00:27 +0100

    fix elementwiseproduct re-loading

--- mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/ElementWiseProductParitySpec.scala
+++ mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/ElementWiseProductParitySpec.scala
@@ -13,6 +13,9 @@ class ElementWiseProductParitySpec extends SparkParityBase {
   override val dataset: DataFrame = baseDataset.select("dti", "loan_amount")
   override val sparkTransformer: Transformer = new Pipeline().setStages(Array(new VectorAssembler().
     setInputCols(Array("dti", "loan_amount")).setOutputCol("features"),
-    new ElementwiseProduct().setInputCol("features").setOutputCol("scaled_features").setScalingVec(Vectors.dense(1.3, 3.4)))).
+    new ElementwiseProduct()
+      .setInputCol("features")
+      .setOutputCol("scaled_features")
+      .setScalingVec(Vectors.dense(1.3, 3.4)))).
     fit(dataset)
 }
