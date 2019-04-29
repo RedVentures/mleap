8511ec05457a513c4653cdcc6edec505699d4cac
asarb <asarb@expedia.com>
2019-04-29 21:15:37 +0100

    amend parity test for BucketedRandomProjectionLSH

--- mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/BucketedRandomProjectionLSHParitySpec.scala
+++ mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/BucketedRandomProjectionLSHParitySpec.scala
@@ -17,4 +17,7 @@ class BucketedRandomProjectionLSHParitySpec extends SparkParityBase {
       setInputCol("features").
       setBucketLength(2).
       setOutputCol("lsh_features"))).fit(dataset)
+
+  override val paramsToSkipTesting = Array("seed") ++ // only affect fitting
+    Array("numHashTables") // we load the randUnitVectors array directly so we don't need this param
 }
