218ede192929ab79f41464b4c9dea82d654ca773
asarb <asarb@expedia.com>
2019-04-30 11:33:24 +0100

    update word to vector parity test

--- mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/WordToVectorParitySpec.scala
+++ mleap-spark/src/test/scala/org/apache/spark/ml/parity/feature/WordToVectorParitySpec.scala
@@ -14,11 +14,9 @@ class WordToVectorParitySpec extends SparkParityBase {
     setInputCol("loan_title").
     setOutputCol("loan_title_tokens"),
     new Word2Vec(uid = "words").
-      setInputCol("loan_title_tokens").setVectorSize(1234)
+      setInputCol("loan_title_tokens")
       setOutputCol("loan_title_token_counts"))).fit(dataset)
 
   // only affect fitting
-  override val paramsToSkipTesting = Array("maxSentenceLength", "maxIter", "seed",
-    "minCount", "numPartitions", "stepSize")
-
+  override val paramsToSkipTesting = Array("seed")
 }
